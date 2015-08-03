a46f0cc7b070f32a5bdc8662b7c78247f38a15c4
古映杰 <yj_gu@Ctrip.com>
2015-08-03 15:37:55 +0800

    update

--- test/test.js
+++ test/test.js
@@ -169,78 +169,181 @@ describe('Process', function() {
             assert.equal(process.resolve('add4', -4), 0)
         })
         it('should "调度器之事件循环"', function() {
-        	var add = function(num) {
-        		return num + 1
-        	}
-        	var dispatcher = {
-        		goTo: function(value) {
-        			return value < 100 ? 'toHundred' : null
-        		}
-        	}
-        	var process = new Process({
-        		toHundred: [add, add, dispatcher],
-        	})
-        	assert.equal(process.resolve('toHundred', 0), 100)
+            var add = function(num) {
+                return num + 1
+            }
+            var dispatcher = {
+                goTo: function(value) {
+                    return value < 100 ? 'toHundred' : null
+                }
+            }
+            var process = new Process({
+                toHundred: [add, add, dispatcher],
+            })
+            assert.equal(process.resolve('toHundred', 0), 100)
         })
         it('should "调度器之异步事件循环"', function(done) {
-        	var add = function(num) {
-        		return num + 1
-        	}
-        	var asyncAdd = function(num) {
-        		return new Promise(function(resolve) {
-        			resolve(add(num))
-        		})
-        	}
-        	var dispatcher = {
-        		goTo: function(value) {
-        			return value < 100 ? 'toHundred' : null
-        		}
-        	}
-        	var process = new Process({
-        		toHundred: [add, asyncAdd, dispatcher],
-        	})
-        	process.resolve('toHundred', 0).then(function(result) {
-        		assert.equal(result, 100)
-        		done()
-        	})
+            var add = function(num) {
+                return num + 1
+            }
+            var asyncAdd = function(num) {
+                return new Promise(function(resolve) {
+                    resolve(add(num))
+                })
+            }
+            var dispatcher = {
+                goTo: function(value) {
+                    return value < 100 ? 'toHundred' : null
+                }
+            }
+            var process = new Process({
+                toHundred: [add, asyncAdd, dispatcher],
+            })
+            process.resolve('toHundred', 0).then(function(result) {
+                assert.equal(result, 100)
+                done()
+            })
         })
         it('should "普通队列里的 promise 异步处理器"', function(done) {
-        	var factory = function() {
-        		return new Promise(function(resolve) {
-        			resolve(function(num) {
-        				return num + 1
-        			})
-        		})
-        	}
+            var factory = function() {
+                return new Promise(function(resolve) {
+                    resolve(function(num) {
+                        return num + 1
+                    })
+                })
+            }
 
-        	var process = new Process({
-        		add3: [factory(), factory(), factory()]
-        	})
+            var process = new Process({
+                add3: [factory(), factory(), factory()]
+            })
 
-        	process.resolve('add3', 0).then(function(result) {
-        		assert.equal(result, 3)
-        		done()
-        	})
+            process.resolve('add3', 0).then(function(result) {
+                assert.equal(result, 3)
+                done()
+            })
         })
         it('should "调度器里的 promise 异步处理器"', function(done) {
-        	var add = function(num) {
-        		return num + 1
-        	}
-        	var dispatcher = {
-        		goTo: function(value) {
-        			return new Promise(function(resolve) {
-        				resolve(value < 100 ? 'add' : value)
-        			})
-        		}
-        	}
-        	var process = new Process({
-        		add: [add, dispatcher]
-        	})
+            var add = function(num) {
+                return num + 1
+            }
+            var dispatcher = {
+                goTo: function(value) {
+                    return new Promise(function(resolve) {
+                        resolve(value < 100 ? 'add' : value)
+                    })
+                }
+            }
+            var process = new Process({
+                add: [add, dispatcher]
+            })
 
-        	process.resolve('add', 12).then(function(result) {
-        		assert.equal(result, 100)
-        		done()
-        	})
+            process.resolve('add', 12).then(function(result) {
+                assert.equal(result, 100)
+                done()
+            })
         })
     })
+
+    describe('#willResolve(eventName)', function() {
+        it('should "输入 eventName，返回只接受 value 参数的 resolve 函数"', function() {
+            var process = new Process({
+                add: function(num) {
+                    return num + 10
+                }
+            })
+            var willAdd = process.willResolve('add')
+            assert(willAdd(1), 11)
+        })
+    })
+
+    describe('#reject(errorName, value)', function() {
+        it('should "进入错误处理进程"', function() {
+            var process = new Process({
+                error: {
+                    "404": function(value) {
+                        return value + ' is not found'
+                    }
+                }
+            })
+            assert.equal(process.reject('404', 123), '123 is not found')
+        })
+        it('should "异步的错误处理进程"', function(done) {
+            var process = new Process({
+                error: {
+                    "404": function(value) {
+                        return new Promise(function(resolve, reject) {
+                            setTimeout(function() {
+                                resolve(value + ' is not found')
+                            }, 2)
+                        })
+                    }
+                }
+            })
+            process.reject('404', 123).then(function(result) {
+                assert.equal(result, '123 is not found')
+                done()
+            })
+        })
+    })
+
+    describe('#willReject(errorName, value)', function() {
+        it('should "输入 errorName， 返回只接受 value 参数的 reject 函数', function() {
+            var process = new Process({
+                error: {
+                    "404": function(value) {
+                        return value + ' is not found'
+                    }
+                }
+            })
+            var willNotFound = process.willReject('404')
+            assert.equal(willNotFound(123), '123 is not found')
+        })
+    })
+
+    describe('嵌套 Process 实例', function() {
+        it('should "从父 process 同步传值到子 process"', function() {
+            var child = new Process({
+                add: function(num) {
+                    return num + 1
+                }
+            })
+
+            var parent = new Process({
+                add: [child.willResolve('add'), function(num) {
+                    return num + 2
+                }]
+            })
+
+            assert.equal(parent.resolve('add', 1), 4)
+        })
+
+        it('should "从父 process 异步传值到子 process"', function(done) {
+            var child = new Process({
+                add: function(num) {
+                    return new Promise(function(resolve, reject) {
+                        setTimeout(function() {
+                            resolve(num + 1)
+                        }, 2)
+                    })
+                }
+            })
+
+            var parent = new Process({
+                add: [child.willResolve('add'), function(num) {
+                    return new Promise(function(resolve, reject) {
+                        setTimeout(function() {
+                            resolve(num + 2)
+                        }, 2)
+                    })
+                }]
+            })
+
+            parent.resolve('add', 1).then(function(result) {
+                assert.equal(result, 4)
+                done()
+            })
+        })
+
+    })
+
 })
